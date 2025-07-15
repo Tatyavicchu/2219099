881246af45744b3f412ecfca1137d83542ea0291
Mohit Bhandari <112754295+Tatyavicchu@users.noreply.github.com>
2025-07-15 12:28:36 +0530

    Final full stack submission with backend, frontend, and logging middleware

--- BackendTestSubmission/index.js
+++ BackendTestSubmission/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { setToken, log } = require('../LoggingMiddleware/logger-backend');
+const { setToken, log } = require('../LoggingMiddleware/loggerbackend');
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
-setToken('PASTE_YOUR_BEARER_TOKEN_HERE');
+setToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJtb2hpdGJoYW5kYXJpMjAwNTdAZ21haWwuY29tIiwiZXhwIjoxNzUyNTYxMzY0LCJpYXQiOjE3NTI1NjA0NjQsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiJkOGE4OTg3Mi0wYjlhLTRlMmUtYTdmNi02ZDg2ZjFiOTUzYmEiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJtb2hpdCBiaGFuZGFyaSIsInN1YiI6IjliOGMxMGI1LTMzNDEtNGM0My04MjI4LTllNzY1OTdjNThjMiJ9LCJlbWFpbCI6Im1vaGl0YmhhbmRhcmkyMDA1N0BnbWFpbC5jb20iLCJuYW1lIjoibW9oaXQgYmhhbmRhcmkiLCJyb2xsTm8iOiIyMjE5MDk5IiwiYWNjZXNzQ29kZSI6IlFBaERVciIsImNsaWVudElEIjoiOWI4YzEwYjUtMzM0MS00YzQzLTgyMjgtOWU3NjU5N2M1OGMyIiwiY2xpZW50U2VjcmV0IjoiaFJnTVJqcXlXQUVTd25qUyJ9.1YAcNzLCkUu0UCoUu4a8rSDHjtqTSDxfmOysoaOdqG0');
 app.use((req, res, next) => {
   log('backend', 'info', 'middleware', `Incoming request: ${req.method} ${req.url}`);
   next();
